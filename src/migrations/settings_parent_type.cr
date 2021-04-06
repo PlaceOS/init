9c1c5e802846ccbd04aa7708e14bd38c389eb811
Kim Burgess <kim@place.technology>
2021-04-06 18:54:44 +1000

    fix: double serialization of parent type

--- src/migrations/settings_parent_type.cr
+++ src/migrations/settings_parent_type.cr
@@ -10,10 +10,11 @@ module Migrations::SettingParentType
         .filter { |s| s["parent_type"].type_of.eq("NUMBER") }
         .update { |s|
           {% begin %}
+            {% type = PlaceOS::Model::Settings::ParentType %}
             r.branch(
-              {% for t in PlaceOS::Model::Settings::ParentType.constants %}
-                s["parent_type"].eq(PlaceOS::Model::Settings::ParentType::{{t}}.to_i),
-                {:parent_type => PlaceOS::Model::Settings::ParentType::{{t}}.to_json},
+              {% for t in type.constants %}
+                s["parent_type"].eq({{type}}::{{t}}.to_i),
+                {:parent_type => JSON.parse({{type}}::{{t}}.to_json)},
               {% end %}
             nil
             )
