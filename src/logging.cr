d207a3b713535e47d3ca8fa2cca2d444ba752aee
Caspian Baska <email@caspian.computer>
2021-12-16 14:38:42 +1100

    fix(logging): prevent log output for LOG_LEVEL=NONE

--- src/logging.cr
+++ src/logging.cr
@@ -1,14 +1,19 @@
 require "placeos-log-backend"
 require "log_helper"
 
-module PlaceOS::Init::Logging
-  ::Log.progname = NAME
-  log_level = PlaceOS.production? ? Log::Severity::Info : Log::Severity::Debug
-  ::Log.setup_from_env(
-    default_sources: "*",
-    default_level: log_level,
-    backend: PlaceOS::LogBackend.log_backend,
-  )
-end
+::Log.progname = PlaceOS::NAME
+log_level = PlaceOS.production? ? Log::Severity::Info : Log::Severity::Debug
+
+backend = if ENV["LOG_LEVEL"]? == "NONE"
+            ::Log::IOBackend.new(File.open(File::NULL, "w"))
+          else
+            PlaceOS::LogBackend.log_backend
+          end
+
+::Log.setup_from_env(
+  default_sources: "*",
+  default_level: log_level,
+  backend: backend
+)
 
 require "./constants"
