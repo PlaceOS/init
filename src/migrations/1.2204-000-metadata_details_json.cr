96613af8ae39426690095652da130cbebb14e8f8
Caspian Baska <email@caspian.computer>
2022-04-08 15:56:02 +1000

    fix(migration): patch to allow for coerced value

--- src/migrations/1.2204-000-metadata_details_json.cr
+++ src/migrations/1.2204-000-metadata_details_json.cr
@@ -1,13 +1,19 @@
 require "../migration"
 
+module RethinkDB
+  def self.json(json_string)
+    DatumTerm.new(TermType::JSON, [json_string])
+  end
+end
+
 module Migrations::MetadataJsonDetails
   include Migration::Irreversible
 
   def self.up
     PlaceOS::Model::Metadata.table_query do |q|
       q
-        .filter { |metadata| metadata["details"].type_of.eq("STRING") }
-        .update { |metadata| ({"details" => r.json(metadata["details"])}) }
+        .filter(&.["details"].type_of.eq("STRING"))
+        .update { |metadata| ({"details" => r.json(metadata["details"].coerce_to("string"))}) }
     end
   rescue e
     Log.error(exception: e) { "failed to migrate Metadata.details to JSON" }
