5af40bfd6b19d1b2679c1c465695cc07387b0e0f
Caspian Baska <email@caspian.computer>
2022-04-20 19:12:56 +1000

    feat(tasks:start): synchronize the start job

--- src/tasks/initialization.cr
+++ src/tasks/initialization.cr
@@ -1,9 +1,17 @@
+require "rethinkdb-orm"
+require "rethinkdb-orm/lock"
+
 require "../logging"
 require "./entities"
 
 module PlaceOS::Tasks::Initialization
   extend self
   Log = ::Log.for(self)
+
+  # Ensures only _one_ start process runs at once
+  protected class_getter start_lock : RethinkORM::Lock do
+    RethinkORM::Lock.new("placeos_start_lock")
+  end
 
   # Initialization script
   def start(
@@ -22,28 +30,31 @@ module PlaceOS::Tasks::Initialization
     application_base = "#{tls ? "https" : "http"}://#{domain}"
     metrics_url = "#{application_base}/#{metrics_route}/"
     metrics_config = {"metrics" => JSON::Any.new(metrics_url)}
-    authority = Entities.create_authority(name: application_name, domain: application_base, config: metrics_config)
 
-    Entities.create_user(authority: authority, name: username, email: email, password: password, sys_admin: true)
-    Entities.create_application(authority: authority, name: application_name, base: application_base)
+    start_lock.synchronize do
+      authority = Entities.create_authority(name: application_name, domain: application_base, config: metrics_config)
 
-    if analytics_route
-      analytics_redirect_uri = File.join(application_base, analytics_route, ANALYTICS_CALLBACK_PATH)
-      Entities.create_application(authority: authority, name: analytics_route, base: application_base, redirect_uri: analytics_redirect_uri)
-    end
+      Entities.create_user(authority: authority, name: username, email: email, password: password, sys_admin: true)
+      Entities.create_application(authority: authority, name: application_name, base: application_base)
 
-    Entities.create_interface(
-      name: "Backoffice",
-      folder_name: "backoffice",
-      uri: "https://github.com/placeos/backoffice",
-      branch: backoffice_branch,
-      commit_hash: backoffice_commit,
-      description: "Admin interface for PlaceOS",
-    )
+      if analytics_route
+        analytics_redirect_uri = File.join(application_base, analytics_route, ANALYTICS_CALLBACK_PATH)
+        Entities.create_application(authority: authority, name: analytics_route, base: application_base, redirect_uri: analytics_redirect_uri)
+      end
 
-    unless PlaceOS::Tasks.production?
-      Log.info { "creating placeholder documents" }
-      Entities.create_placeholders
+      Entities.create_interface(
+        name: "Backoffice",
+        folder_name: "backoffice",
+        uri: "https://github.com/placeos/backoffice",
+        branch: backoffice_branch,
+        commit_hash: backoffice_commit,
+        description: "Admin interface for PlaceOS",
+      )
+
+      unless PlaceOS::Tasks.production?
+        Log.info { "creating placeholder documents" }
+        Entities.create_placeholders
+      end
     end
   end
 end
