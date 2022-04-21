8001c7554b3482ee17576788535ea21be9729139
Caspian Baska <email@caspian.computer>
2022-04-21 10:34:22 +1000

    fix(migrations): lock migration task

--- src/migrations.cr
+++ src/migrations.cr
@@ -1,10 +1,15 @@
+require "rethinkdb-orm"
+require "rethinkdb-orm/lock"
+
 require "./migrations/*"
 
 module Migrations
   def self.apply_all
-    {% for migration in Migration.includers.sort_by &.constant(:Ref) %}
-      # FIXME: run version diffing for direction. Up only for now.
-      {{migration}}.up
-    {% end %}
+    RethinkORM::Lock.new("placeos-migrations").synchronize do
+      {% for migration in Migration.includers.sort_by &.constant(:Ref) %}
+        # FIXME: run version diffing for direction. Up only for now.
+        {{migration}}.up
+      {% end %}
+    end
   end
 end
