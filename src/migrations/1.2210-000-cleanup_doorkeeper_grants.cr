f8d71f6e39dca57a18f4d2bcb5abf6346cf75ee7
Mia Bennett <mia@chillfox.com>
2022-09-14 11:37:49 +0930

    fix(grant): remove doorkeeper grants without a ttl (#74)
    * fix(grant): remove doorkeeper grants without a ttl
    
    * fix(doorkeeper): ttl query

---
+++ src/migrations/1.2210-000-cleanup_doorkeeper_grants.cr
@@ -0,0 +1,16 @@
+require "../migration"
+
+module Migrations::CleanupDoorkeeperGrants
+  include Migration::Irreversible
+
+  def self.up
+    raw_query do |r|
+      r
+        .table("doorkeeper_grant")
+        .filter(&.has_fields("ttl").not)
+        .delete
+    end
+  rescue e
+    Log.error(exception: e) { "failed to remove doorkeeper grants without a ttl" }
+  end
+end
