4904ed58d9050d37593583fd61dbe1239751f490
Stephen von Takach <steve@place.technology>
2023-05-09 17:11:25 +1000

    drop a second unique clear index
    only defined in clear

--- db/migrations/20230509143734_alter_staff_api.sql
+++ db/migrations/20230509143734_alter_staff_api.sql
@@ -4,6 +4,7 @@
  ALTER TYPE survey_trigger_type ADD VALUE 'VISITOR_CHECKEDIN';
  ALTER TYPE survey_trigger_type ADD VALUE 'VISITOR_CHECKEDOUT';
  DROP INDEX IF EXISTS index_event_metadatas_event_id;
+ DROP INDEX IF EXISTS event_metadatas_event_id;
  
 -- +micrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
