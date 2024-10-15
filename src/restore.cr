6866c513296b228a91be1a8c9404ed904383a138
Ali Naqvi <naqvis@users.noreply.github.com>
2024-10-15 13:22:58 +0800

    feat: PPT-1516 Add support for Azure Blob storage to init (#89)

--- src/restore.cr
+++ src/restore.cr
@@ -2,18 +2,33 @@ require "./tasks"
 require "./logging"
 
 module PlaceOS
-  Tasks::Restore.pg_restore(
-    pg_host: PG_HOST,
-    pg_port: PG_PORT,
-    pg_db: PG_DB,
-    pg_user: PG_USER,
-    pg_password: PG_PASS,
-    force_restore: PG_FORCE_RESTORE,
-    aws_s3_object: AWS_S3_OBJECT || abort("AWS_S3_OBJECT is unset"),
-    aws_region: AWS_REGION || abort("AWS_REGION is unset"),
-    aws_key: AWS_KEY || abort("AWS_KEY is unset"),
-    aws_secret: AWS_SECRET || abort("AWS_SECRET is unset"),
-    aws_s3_bucket: AWS_S3_BUCKET || abort("AWS_S3_BUCKET is unset"),
-    aws_kms_key_id: AWS_KMS_KEY_ID,
-  )
+  if (AZURE_STORAGE_ACCOUNT_NAME && AZURE_STORAGE_ACCOUNT_KEY) || AZURE_STORAGE_CONNECTION_STRING
+    Tasks::Restore.az_restore(
+      pg_host: PG_HOST,
+      pg_port: PG_PORT,
+      pg_db: PG_DB,
+      pg_user: PG_USER,
+      pg_password: PG_PASS,
+      az_account: AZURE_STORAGE_ACCOUNT_NAME,
+      az_key: AZURE_STORAGE_ACCOUNT_KEY,
+      az_connstr: AZURE_STORAGE_CONNECTION_STRING,
+      az_container: AZURE_STORAGE_CONTAINER || abort("AZURE_STORAGE_CONTAINER is unset"),
+      az_blob_object: AZURE_STORAGE_BLOB_OBJECT || abort("AZURE_STORAGE_BLOB_OBJECT unset")
+    )
+  else
+    Tasks::Restore.pg_restore(
+      pg_host: PG_HOST,
+      pg_port: PG_PORT,
+      pg_db: PG_DB,
+      pg_user: PG_USER,
+      pg_password: PG_PASS,
+      force_restore: PG_FORCE_RESTORE,
+      aws_s3_object: AWS_S3_OBJECT || abort("AWS_S3_OBJECT is unset"),
+      aws_region: AWS_REGION || abort("AWS_REGION is unset"),
+      aws_key: AWS_KEY || abort("AWS_KEY is unset"),
+      aws_secret: AWS_SECRET || abort("AWS_SECRET is unset"),
+      aws_s3_bucket: AWS_S3_BUCKET || abort("AWS_S3_BUCKET is unset"),
+      aws_kms_key_id: AWS_KMS_KEY_ID,
+    )
+  end
 end
--- src/tasks/backup.cr
+++ src/tasks/backup.cr
@@ -3,6 +3,7 @@ require "../logging"
 require "exec_from"
 require "retriable/core_ext/kernel"
 require "tasker"
+require "azblob"
 
 require "../utils/postgres"
 require "../utils/s3"
@@ -57,6 +58,51 @@ module PlaceOS::Tasks::Backup
     end
   end
 
+  # Backup PostgreSQL database to Azure Storage
+  #
+  def az_backup(
+    pg_host : String,
+    pg_port : Int32,
+    az_account : String?,
+    az_key : String?,
+    az_connstr : String?,
+    az_container : String,
+    pg_db : String? = nil,
+    pg_user : String? = nil,
+    pg_password : String? = nil
+  )
+    Log.context.set(
+      pg_host: pg_host,
+      pg_port: pg_port,
+      pg_db: pg_db || "Full backup",
+      az_container: az_container
+    )
+
+    abort "Neither arguments were given nor AZURE_STORAGE_XXXX environment variables were set" unless (az_account && az_key) || az_connstr
+    client = if str = az_connstr
+               AZBlob.client(str)
+             else
+               AZBlob::Client.new(AZBlob::Client::Config.with_shared_key(az_account.not_nil!, az_key.not_nil!))
+             end
+
+    Log.info { "running postgresql backup" }
+    path = PlaceOS::Utils::PostgresDB.dump(
+      host: pg_host,
+      port: pg_port,
+      db: pg_db.not_nil!,
+      user: pg_user.not_nil!,
+      password: pg_password.not_nil!,
+    )
+
+    if path
+      File.open(path) do |file|
+        client.put_blob(az_container, path.basename, file)
+      end
+    else
+      Log.error { "failed to capture postgresql backup" }
+    end
+  end
+
   # Periodically backup postgres to s3
   #
   def pg_backup_cron(
@@ -102,5 +148,54 @@ module PlaceOS::Tasks::Backup
     end
 
     writer.process!
+  end
+
+  # Periodically backup postgres to Azure Storage
+  #
+  def az_backup_cron(
+    pg_host : String,
+    pg_port : Int32,
+    az_account : String?,
+    az_key : String?,
+    az_connstr : String?,
+    az_container : String,
+    pg_db : String? = nil,
+    pg_user : String? = nil,
+    pg_password : String? = nil,
+    cron : String = BACKUP_CRON
+  )
+    Log.context.set(
+      pg_host: pg_host,
+      pg_port: pg_port,
+      pg_db: pg_db,
+      cron: cron,
+    )
+
+    abort "Neither arguments were given nor AZURE_STORAGE_XXXX environment variables were set" unless (az_account && az_key) || az_connstr
+    client = if str = az_connstr
+               AZBlob.client(str)
+             else
+               AZBlob::Client.new(AZBlob::Client::Config.with_shared_key(az_account.not_nil!, az_key.not_nil!))
+             end
+
+    Log.info { "starting backup cron - azure" }
+    Tasker.cron(cron) do
+      Log.info { "running postgres backup" }
+
+      path = PlaceOS::Utils::PostgresDB.dump(
+        host: pg_host,
+        port: pg_port,
+        db: pg_db.not_nil!,
+        user: pg_user.not_nil!,
+        password: pg_password.not_nil!,
+      )
+      if path
+        File.open(path) do |file|
+          client.put_blob(az_container, path.basename, file)
+        end
+      else
+        Log.error { "failed to capture postgresql backup" }
+      end
+    end
   end
 end
