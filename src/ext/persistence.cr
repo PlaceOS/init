9f17e1eaedaa2a22a4245488a0e5e3b77405d90e
Ali Naqvi <naqvis@users.noreply.github.com>
2025-04-16 13:34:33 +0800

    fix(entities): fix + upgrade to pg-orm v2 (#98)

--- src/ext/persistence.cr
+++ src/ext/persistence.cr
@@ -1,16 +1,24 @@
 # nodoc:
 module PgORM::Persistence
   private def __create(**options)
-    builder = Query::Builder.new(table_name, primary_key.to_s)
+    builder = Query::Builder.new(table_name, primary_key.first.to_s)
     adapter = Database.adapter(builder)
 
     Database.transaction do
       raise PgORM::Error::RecordInvalid.new(self) unless valid?
       attributes = self.persistent_attributes
-      attributes.delete(primary_key) unless self.id?
+      keys = primary_key
+      vals = self.id?
+      case vals
+      when Nil
+        keys.each { |key| attributes.delete(key) }
+      when Enumerable
+        primary_key.each_with_index { |key, index| attributes.delete(key) if vals[index].nil? }
+      end
+
       begin
         adapter.insert(attributes) do |rid|
-          set_primary_key_after_create(rid) unless self.id?
+          set_primary_key_after_create(rid)
           clear_changes_information
           self.new_record = false
         end
